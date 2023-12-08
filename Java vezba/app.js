24916262ac51e3190d7379571e432aafb435ed91
OlgicaCh <150544326+OlgicaCh@users.noreply.github.com>
2023-12-08 13:28:27 +0100

    Add files via upload

---
+++ Java vezba/app.js
@@ -0,0 +1,12 @@
+console.log("stay in a life");
+let userInput = prompt("How much mony do you have?");
+let amount = Number(userInput);
+let useramount = 15;
+let user = true;
+if (amount >= 50 && user) {
+  console.log("Pay your bills");
+} else if (amount >= 35 && user) {
+  console.log("Have I nice dinner");
+} else if (amount >= 11 && user) {
+  console.log(" Take a day off");
+}
