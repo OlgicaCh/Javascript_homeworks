ed6671f33fc441491998093a356c2b5dcc0dc1c7
OlgicaCh <150544326+OlgicaCh@users.noreply.github.com>
2023-12-09 15:54:25 +0100

    Add files via upload

---
+++ Function-type/app.js
@@ -0,0 +1,42 @@
+console.log("working");
+
+//function-type- Number
+
+function number() {
+  let result = 7 + 5;
+  console.log(result);
+  console.log(typeof result);
+}
+number();
+
+// string
+function myNameIs() {
+  let name = "Olgica";
+  console.log(name);
+  console.log(typeof name);
+}
+myNameIs();
+
+// boolean
+function boolean() {
+  let compare = 5 > 10;
+  console.log(compare);
+  console.log(typeof compare);
+}
+boolean();
+
+//null
+function inputStudentname() {
+  let studentName = null;
+  console.log(studentName);
+  console.log(typeof studentName);
+}
+inputStudentname();
+
+//undefined
+function calculateAverage() {
+  let average;
+  console.log(average);
+  console.log(typeof average);
+}
+calculateAverage();
